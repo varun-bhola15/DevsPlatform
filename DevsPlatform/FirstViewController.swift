a359a7ac8ec51f9e8fad0cbf9ee8fa44f0591278
varun-bhola15 <varundude.bhola@gmail.com>
2018-01-11 15:26:13 +0530

    Initial Commit

---
+++ DevsPlatform/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  DevsPlatform
+//
+//  Created by varun bhola on 11/01/18.
+//  Copyright © 2018 varun15bhola. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
